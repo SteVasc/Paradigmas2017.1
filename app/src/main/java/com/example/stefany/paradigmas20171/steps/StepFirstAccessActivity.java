075c7c594a4100cd5c50a3fe38fc2e6d0352214d
Reddstar <ricardolucas1196@gmail.com>
2017-06-08 21:30:21 -0300

    Changing to activity, step 4 of 4

--- app/src/main/java/com/example/stefany/paradigmas20171/steps/StepFirstAccessActivity.java
+++ app/src/main/java/com/example/stefany/paradigmas20171/steps/StepFirstAccessActivity.java
@@ -5,11 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.example.stefany.paradigmas20171.R;
 
-/**
- * Created by laser info on 08/06/2017.
- */
-
 public class StepFirstAccessActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
